f94cb5f23cebb218c2aaa3874db599caeeaaf2cb
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2023-08-06 21:09:40 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+let email = document.getElementById('email'),
+password = document.getElementById('password'),
+
+
+function signUpButtonPressed(){
+    firebase.auth().createUserWithEmailAndPassword(email.Value, password.value).then(auth => {
+        console.log(auth);
+    }).catch(error => {
+        console.log(error.message);
+    })
+}
